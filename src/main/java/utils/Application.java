23d0fe9d134c8915e471ebed027951db928fbb3d
rickysun93 <rickysun93@outlook.com>
2016-10-20 13:51:56 +0800

    初始化接口

---
+++ src/main/java/utils/Application.java
@@ -0,0 +1,16 @@
+package utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sunhaoran on 2016/10/19.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
