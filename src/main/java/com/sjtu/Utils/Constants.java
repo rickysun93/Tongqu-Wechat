34670ea02bc290961e9407ca296cc9292612dd70
rickysun93 <rickysun93@outlook.com>
2018-03-20 15:17:45 +0800

    更新url

--- src/main/java/com/sjtu/Utils/Constants.java
+++ src/main/java/com/sjtu/Utils/Constants.java
@@ -5,7 +5,7 @@ package com.sjtu.Utils;
  */
 public class Constants {
     public static String Wechaturl = "https://api.weixin.qq.com";
-    public static String Tongquurl = "http://www.tongqu.me/api/act/type?type=2&status=0&order=act.create_time";
+    public static String Tongquurl = "https://tongqu.me/api/act/type?type=2&status=0&order=act.create_time";
 
 //    public static String Appid = "wx8e875b601a088908";
 //    public static String Appsecret = "45f0809aabed5bd2597256525f9668b2";
@@ -13,5 +13,5 @@ public class Constants {
     public static String Appid = "wxc32b3ddc0cce2f08";
     public static String Appsecret = "0d2a33a94ac1b0fd3515594b2ca96c51";
 
-    public static String Latestactid = "13571";
+    public static String Latestactid = "16735";
 }
